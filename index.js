48c923bd36e1c1c637986dab9c615515b6adc1e5
Ishan1210 <ishangrover121005@gmail.com>
2026-02-04 10:21:15 +0530

    text file

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
-const http=require('http');
-const myserver=http.createServer((req,res)=>{
-    // console.log('server1');
-    // res.end('Hello from server1');
-    if(req.url=='/'){
-        res.writeHead(200, { "Content-Type": "text/html" });
+// const http=require('http');
+// const myserver=http.createServer((req,res)=>{
+//     // console.log('server1');
+//     // res.end('Hello from server1');
+//     if(req.url=='/'){
+//         res.writeHead(200, { "Content-Type": "text/html" });
 
-        res.end(`
-                <img src="https://th.bing.com/th/id/OIP._mVfeIz_M5OghcDn9HD8RgAAAA?w=121&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" alt="ABES Logo"/>
-                <h1>We are students of ABES</h1>
-                    `);
+//         res.end(`
+//                 <img src="https://th.bing.com/th/id/OIP._mVfeIz_M5OghcDn9HD8RgAAAA?w=121&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" alt="ABES Logo"/>
+//                 <h1>We are students of ABES</h1>
+//                     `);
 
-    }
-    else if(req.url=='/about'){
-        res.end('We are students of ABESEC');
-    } 
-    else if(req.url=='/contact'){
-        res.end('Student Contact: 1234567890');
-    }
-    else if(req.url=='/class'){
-        res.end('Class is IT-A');
-    }  
-    else{
-        res.end('404 Page Not Found');
-    }
-});
-myserver.listen(3000,()=>{
-    console.log('Server is listening on port 3000');
-});
+//     }
+//     else if(req.url=='/about'){
+//         res.end('We are students of ABESEC');
+//     } 
+//     else if(req.url=='/contact'){
+//         res.end('Student Contact: 1234567890');
+//     }
+//     else if(req.url=='/class'){
+//         res.end('Class is IT-A');
+//     }  
+//     else{
+//         res.end('404 Page Not Found');
+//     }
+// });
+// myserver.listen(3000,()=>{
+//     console.log('Server is listening on port 3000');
+// });
+
+const fs=require('fs');
+fs.writeFileSync('./it-a.txt','We are student of IT');
+fs.writeFileSync('./ABES.txt','We are student of ABES');
+
 
