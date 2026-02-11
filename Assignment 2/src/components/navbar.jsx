a50453718e1a7cfae78829b93f8f11a8f418e38f
Ishan1210 <ishangrover121005@gmail.com>
2026-02-11 10:13:47 +0530

    assignment 2

---
+++ Assignment 2/src/components/navbar.jsx
@@ -0,0 +1,29 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img
+          src="https://th.bing.com/th/id/OIP._mVfeIz_M5OghcDn9HD8RgAAAA?w=121&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3"
+          alt="Logo"
+        />
+        <span>Assignment_1</span>
+      </div>
+
+      <ul className="nav-links">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
